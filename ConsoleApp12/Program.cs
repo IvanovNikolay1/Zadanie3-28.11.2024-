dda36cba8dfd972edb1c17bb82070ede8d7f155d
IvanovNR <IvanovNR@310D-12>
2024-12-05 10:08:14 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        public static long SquareDigits(long n)
+        {
+            string numberString = n.ToString();
+            string result = "";
+
+            foreach (char digit in numberString)
+            {
+                int digitValue = digit - '0';
+                int square = digitValue * digitValue;
+
+                result += square.ToString();
+            }
+            return long.Parse(result);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            string input = Console.ReadLine();
+
+            if (long.TryParse(input, out long number))
+            {
+                long result = SquareDigits(number);
+                Console.WriteLine($"Результат: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка. Введено не коректное число");
+            }
+        }
+    }
+}
